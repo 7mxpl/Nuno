995536412e54833f29e93b5cd6a2aa7183471189
7mxpl <muelleranton09@gmail.com>
2025-04-25 14:57:46 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
 const channelId = "UCJ6Hx95RAjXplGcqnlLufzw";
-const apiUrl = `https://api.rss2json.com/v1/api.json?rss_url=https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`;
+const apiUrl = `https://api.rss2json.com/v1/api.json?rss_url=https://www.youtube.com/feeds/videos.xml&channel_id=${channelId}`;
 
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    const latestVideoUrl = data.items[0].link;
-    const linkElement = document.getElementById("videoLink");
-    linkElement.href = latestVideoUrl;
-    linkElement.textContent = "🎬 Zum neuesten YouTube-Video!";
+    if (data.items && data.items.length > 0) {
+      const latestVideoUrl = data.items[0].link;
+      const linkElement = document.getElementById("videoLink");
+      linkElement.href = latestVideoUrl;
+      linkElement.textContent = "🎬 Zum neuesten YouTube-Video!";
+    } else {
+      document.getElementById("videoLink").textContent = "Keine Videos verfügbar.";
+    }
   })
   .catch(error => {
     console.error("Error:", error);
